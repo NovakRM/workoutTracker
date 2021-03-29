c9fe314b2cf322207286ec39f72a537511baebc7
osteophagy <mnovakahs@gmail.com>
2021-03-29 19:55:13 -0400

    Deploying

--- server.js
+++ server.js
@@ -16,7 +16,18 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutDBApp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // API 
 
