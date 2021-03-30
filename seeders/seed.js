ab1536574f816b2fe5140ec29c313aeebfae161c
osteophagy <mnovakahs@gmail.com>
2021-03-29 20:04:42 -0400

    Trying to get modules consistent per heroku build error logs.

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models/Index");
+let db = require("../models");
 
 mongoose.connect("mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
